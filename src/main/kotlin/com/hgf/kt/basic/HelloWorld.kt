b5cd443e6398ae027f906b675e7b6f52b07dcb42
GentleHe <13789127308@163.com>
2024-03-15 16:43:51 +0800

    提交

---
+++ src/main/kotlin/com/hgf/kt/basic/HelloWorld.kt
@@ -0,0 +1,11 @@
+package com.hgf.kt.basic
+
+class HelloWorld {
+    fun main(args: Array<String>){
+        println("Hello World")
+    }
+}
+
+fun main(args: Array<String>){
+    println("Hello World")
+}
